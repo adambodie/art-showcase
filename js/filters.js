89d6326d1dac36686f71c9411e1992d271193578
Adam Bodie <aduo3000@yahoo.com>
2017-01-08 13:10:48 -0800

    Revert "fixed transform CSS, broken List.js links"
    This reverts commit 0a505d310b0851a58425adef559e0cb5c74185ae.
    
    # Conflicts:
    #	css/main.css
    #	css/showcase.css
    #	js/filters.js
    #	showcase.html

--- js/filters.js
+++ js/filters.js
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
 		var options = {
-		valueNames: [ 'name'],
-		page: 12,
-		plugins: [
-			ListPagination({outerWindow: 10}) 
-		]
-	};
-
-	var userList = new List('gallery', options);
-
-=======
-	var options = { 
+		valueNames: [ 'name'], 
 		page: 12, 
 		plugins: [ 
-			ListPagination({}) 
+			ListPagination({outerWindow: 10}) 
 		] 
 	};
 
 	var userList = new List('gallery', options);
 	
->>>>>>> 41a800a66f5eccd16da176aa8682c2fb85eda44f
 		$('.all-filter').on('click', function() {
  		 	userList.filter();
 		});
@@ -43,113 +31,59 @@
       	return false;
     	}
   	});
-<<<<<<< HEAD
-});
-	$('.portrait-filter').on('click', function() {
-		userList.filter(function(item) {
-	 	var portrait = item.elm.className;
-		if (portrait.includes('portrait')) {
-=======
 }); 
 	$('.portrait-filter').on('click', function() {
 		userList.filter(function(item) {
 	 	var portrait = item.elm.className;
 		if (portrait.includes('portrait')) { 
->>>>>>> 41a800a66f5eccd16da176aa8682c2fb85eda44f
       	return true;
     	} else {
       	return false;
     	}
-<<<<<<< HEAD
-  	});
-=======
   	});  	  	
->>>>>>> 41a800a66f5eccd16da176aa8682c2fb85eda44f
 });
 	$('.landscape-filter').on('click', function() {
 		userList.filter(function(item) {
 	 	var landscape = item.elm.className;
-<<<<<<< HEAD
-		if (landscape.includes('landscape')) {
-=======
 		if (landscape.includes('landscape')) { 
->>>>>>> 41a800a66f5eccd16da176aa8682c2fb85eda44f
       	return true;
     	} else {
       	return false;
     	}
-<<<<<<< HEAD
-  	});
-});
-	$('.object-filter').on('click', function() {
-		userList.filter(function(item) {
-	 	var object = item.elm.className;
-		if (object.includes('object')) {
-=======
   	});  	  	
 });  
 	$('.object-filter').on('click', function() {
 		userList.filter(function(item) {
 	 	var object = item.elm.className;
 		if (object.includes('object')) { 
->>>>>>> 41a800a66f5eccd16da176aa8682c2fb85eda44f
       	return true;
     	} else {
       	return false;
     	}
-<<<<<<< HEAD
-  	});
-});
-	$('.miscellaneous-filter').on('click', function() {
-		userList.filter(function(item){
-		var miscellaneous = item.elm.className;
-		if (miscellaneous.includes('miscellaneous')) {
-=======
   	});  	  	
 });  
 	$('.miscellaneous-filter').on('click', function() {
 		userList.filter(function(item){ 
 		var miscellaneous = item.elm.className;
 		if (miscellaneous.includes('miscellaneous')) {    	
->>>>>>> 41a800a66f5eccd16da176aa8682c2fb85eda44f
       	return true;
     	} else {
       	return false;
     	}
-<<<<<<< HEAD
-  	});
-});
-=======
   	});  	  	
 });   
->>>>>>> 41a800a66f5eccd16da176aa8682c2fb85eda44f
 
 
 
 
 	$('.acrylic-filter').on('click', function() {
-<<<<<<< HEAD
-		userList.filter(function(item){
-		var acrylic = item.elm.className;
-		if (acrylic.includes('acrylic paint')) {
-=======
 		userList.filter(function(item){ 
 		var acrylic = item.elm.className;
 		if (acrylic.includes('acrylic paint')) {    	
->>>>>>> 41a800a66f5eccd16da176aa8682c2fb85eda44f
       	return true;
     	} else {
       	return false;
     	}
-<<<<<<< HEAD
-  	});
-});
-
-	$('.charcoal-filter').on('click', function() {
-		userList.filter(function(item){
-		var charcoal = item.elm.className;
-		if (charcoal.includes('charcoal')) {
-=======
   	});  	  	
 });   
 
@@ -157,20 +91,10 @@
 		userList.filter(function(item){ 
 		var charcoal = item.elm.className;
 		if (charcoal.includes('charcoal')) {    	
->>>>>>> 41a800a66f5eccd16da176aa8682c2fb85eda44f
       	return true;
     	} else {
       	return false;
     	}
-<<<<<<< HEAD
-  	});
-});
-
-	$('.pencil-filter').on('click', function() {
-		userList.filter(function(item){
-		var pencil = item.elm.className;
-		if (pencil.includes('pencil')) {
-=======
   	});  	  	
 });   
 
@@ -178,20 +102,10 @@
 		userList.filter(function(item){ 
 		var pencil = item.elm.className;
 		if (pencil.includes('pencil')) {    	
->>>>>>> 41a800a66f5eccd16da176aa8682c2fb85eda44f
       	return true;
     	} else {
       	return false;
     	}
-<<<<<<< HEAD
-  	});
-});
-
-	$('.pastels-filter').on('click', function() {
-		userList.filter(function(item){
-		var pastels = item.elm.className;
-		if (pastels.includes('color pastels')) {
-=======
   	});  	  	
 });   
 
@@ -199,20 +113,10 @@
 		userList.filter(function(item){ 
 		var pastels = item.elm.className;
 		if (pastels.includes('color pastels')) {    	
->>>>>>> 41a800a66f5eccd16da176aa8682c2fb85eda44f
       	return true;
     	} else {
       	return false;
     	}
-<<<<<<< HEAD
-  	});
-});
-
-	$('.paz-filter').on('click', function() {
-		userList.filter(function(item){
-		var paz = item.elm.className;
-		if (paz.includes('cray-paz')) {
-=======
   	});  	  	
 });   
 
@@ -220,20 +124,10 @@
 		userList.filter(function(item){ 
 		var paz = item.elm.className;
 		if (paz.includes('cray-paz')) {    	
->>>>>>> 41a800a66f5eccd16da176aa8682c2fb85eda44f
       	return true;
     	} else {
       	return false;
     	}
-<<<<<<< HEAD
-  	});
-});
-
-	$('.construction-filter').on('click', function() {
-		userList.filter(function(item){
-		var construction = item.elm.className;
-		if (construction.includes('construction paper')) {
-=======
   	});  	  	
 });   
 
@@ -241,20 +135,10 @@
 		userList.filter(function(item){ 
 		var construction = item.elm.className;
 		if (construction.includes('construction paper')) {    	
->>>>>>> 41a800a66f5eccd16da176aa8682c2fb85eda44f
       	return true;
     	} else {
       	return false;
     	}
-<<<<<<< HEAD
-  	});
-});
-
-	$('.computer-filter').on('click', function() {
-		userList.filter(function(item){
-		var computer = item.elm.className;
-		if (computer.includes('computer')) {
-=======
   	});  	  	
 }); 
 
@@ -262,19 +146,13 @@
 		userList.filter(function(item){ 
 		var computer = item.elm.className;
 		if (computer.includes('computer')) {    	
->>>>>>> 41a800a66f5eccd16da176aa8682c2fb85eda44f
       	return true;
     	} else {
       	return false;
     	}
-<<<<<<< HEAD
-  	});
-});
-=======
   	});  	  	
 });   
 
 
 
  
->>>>>>> 41a800a66f5eccd16da176aa8682c2fb85eda44f
