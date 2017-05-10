f4910c5c4335b6c02c671c409638347d33cd5c28
adambodie <aduo3000@yahoo.com>
2017-05-09 17:59:03 -0700

    Updated App component with basic home/about design

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './css/main.css';
+import './css/about.css';
+import './css/showcase.css';
+import './css/query.css';
 
 ReactDOM.render(
   <App />,
