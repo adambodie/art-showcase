334f0135f0a21ca9f95c5612c99f11d54e74acd6
adambodie <aduo3000@yahoo.com>
2017-05-26 17:45:24 -0700

    Added headers to About page

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,7 @@ import React from 'react'
 const About = () => (
  <div>
 		<div className="about">
+			<h2>About Me</h2>
 			<p>Growing up, I had an artistic side to me that I loved.  Even now, I still have that creative side, albeit in a different form.  My love of art peaked when I was in high school.  Needing a way to express myself, I chose to do so primarily by drawing and painting.  I loved to draw portraits mostly, but managed to find other things to draw.</p>
 			<img src="img/teenager.jpg" alt="Adam" className="img-responsive" id="teenager"/>	
 			<p>Most of my actual drawings and paintings are unfortunately gone.  I didn’t have much use of them at the time and they were taking space.  Especially the big life size nudes I drew attending a course at PNCA when I was 16 years old.  I figured at some point I would regret it, but then again, we all have regrets.  Now I've reached that point, as I discovered my Art Showcase I put together when I was 16.  I managed to save a few of my drawings and paintings from high school, complete with my thoughts at the time.</p>
@@ -11,6 +12,7 @@ const About = () => (
 			<p>With all that said, I am still proud of everything I accomplished back then, even though I didn’t make a career out of it, as I once considered.  I have great memories of showcasing my artwork.  One time I cherish was when I showed my works to the artist in my family, my late cousin Channa Horwitz.  She was probably the most fascinating person in our family, creative and perhaps ahead of her time; her artwork has appeared in museums here and afar.  Though I didn’t see her too many times since she lived in L.A., I still cherish the brief connection we shared.  To this day, I still have the piece of art she gave me.  I also remember the art classes I took in high school at PNCA; among the things I remember was telling my parents that I was drawing people in the nude, and explaining to them the artistic significance of drawing people in the nude, flexing my skills with artistic factuality. That made me feel like an art nerd.</p>
 		</div>
 		<div className="initial">
+		<h2>My Initial Thoughts</h2>
 		<p id="initial">Here were my initial thoughts from my art showcase back in 2001-2002.  As you can see, a lot has changed since then, among the notable changes was that I actually had hair when I was young.  Other than that, I'm still the same artistic person but with a different career path and improved maturity and personality.</p>
 			<img src="img/initial-thoughts.jpg" alt="Initial thoughts" className="img-responsive" id="thoughts"/>
 		</div>
